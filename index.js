f90ca7a6fbafd2f4959dd8428ce9a1409b4c2207
Lars Kliesing <lgk@live.de>
2022-04-09 20:25:04 +0200

    Battle!

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ const player = new Sprite({
         y: canvas.height / 2 - 68 / 2
     },
     image: playerDownImage,
-    frames: { max: 4 },
+    frames: { max: 4, hold: 10 },
     sprites: {
         up: playerUpImage,
         down: playerDownImage,
@@ -126,7 +126,7 @@ function animate() {
     foreground.draw()
 
     let moving = true
-    player.moving = false
+    player.animate = false
 
     if (battle.initiated) return
 
@@ -166,11 +166,16 @@ function animate() {
                     onComplete: () => {
                         gsap.to("#overlappingDiv", {
                             opacity: 1,
-                            duration: 0.4
+                            duration: 0.4,
+                            onComplete: () => {
+                                // activate a new animation loop
+                                animateBattle()
+                                gsap.to("#overlappingDiv", {
+                                    opacity: 0,
+                                    duration: 0.4
+                                })
+                            }
                         })
-
-                        // activate a new animation loop
-                        animateBattle()
                     }
                 })
 
@@ -180,7 +185,7 @@ function animate() {
     }
 
     if (keys.w.pressed && lastKey === "w") {
-        player.moving = true
+        player.animate = true
         player.image = player.sprites.up
 
         for (let i = 0; i < boundaries.length; i++) {
@@ -206,7 +211,7 @@ function animate() {
             movables.forEach((movable) => (movable.position.y += 3))
         }
     } else if (keys.a.pressed && lastKey === "a") {
-        player.moving = true
+        player.animate = true
         player.image = player.sprites.left
 
         for (let i = 0; i < boundaries.length; i++) {
@@ -231,7 +236,7 @@ function animate() {
             movables.forEach((movable) => (movable.position.x += 3))
         }
     } else if (keys.s.pressed && lastKey === "s") {
-        player.moving = true
+        player.animate = true
         player.image = player.sprites.down
 
         for (let i = 0; i < boundaries.length; i++) {
@@ -256,7 +261,7 @@ function animate() {
             movables.forEach((movable) => (movable.position.y -= 3))
         }
     } else if (keys.d.pressed && lastKey === "d") {
-        player.moving = true
+        player.animate = true
         player.image = player.sprites.right
 
         for (let i = 0; i < boundaries.length; i++) {
@@ -284,9 +289,38 @@ function animate() {
 }
 animate()
 
+const battleBackgroundImage = new Image()
+battleBackgroundImage.src = "./img/battleBackground.png"
+const battleBackground = new Sprite({
+    position: { x: 0, y: 0 },
+    image: battleBackgroundImage
+})
+
+const draggleImage = new Image()
+draggleImage.src = "./img/draggleSprite.png"
+const draggle = new Sprite({
+    position: { x: 800, y: 100 },
+    image: draggleImage,
+    frames: { max: 4, hold: 30 },
+    animate: true
+})
+
+const embyImage = new Image()
+embyImage.src = "./img/embySprite.png"
+const emby = new Sprite({
+    position: { x: 280, y: 325 },
+    image: embyImage,
+    frames: { max: 4, hold: 30 },
+    animate: true
+})
+
 function animateBattle() {
     window.requestAnimationFrame(animateBattle)
+    battleBackground.draw()
+    draggle.draw()
+    emby.draw()
 }
+// animateBattle()
 
 let lastKey = ""
 
